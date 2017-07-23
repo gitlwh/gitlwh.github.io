b7762bcadd1941c4d7eab05558bffd8e8dc65d07
19920919 <wel615@lehigh.edu>
2017-07-23 13:55:46 -0400

    upload paper

--- js/main/app.js
+++ js/main/app.js
@@ -12,31 +12,6 @@ $(function() {
 		source: 'https://github.com/gitlwh/crossBug',
 		type: ''
 	}, {
-		title: 'Botnet Detector',
-		image: 'dist/images/botnetDetector.jpg',
-		description: 'A tool used to detect botnet based on existing P2P botnet packet dataset and health packet dataset. Using machine learning to differentiate botnet trace out of normal trace.',
-		keywords:'Python, Tshark, Numpy, Sklearn',
-		source: 'https://github.com/gitlwh/Botnet-Detector'
-	}, {
-		title: 'User Level Threading Library',
-		image: 'dist/images/userLevelThreadingLibrary.jpg',
-		description: 'A user level threading library with function of create, wait, join, exit, etc.',
-		keywords:'C/C++',
-		source: 'https://github.com/gitlwh/User-Level-Threading-Library'
-	}, {
-		title: 'FUSE Filesystem',
-		image: 'dist/images/FUSEFilesystem.jpg',
-		keywords: 'C/C++',
-		description: 'A file system with the FUSE Userspace filesystem module.',
-		source: 'https://github.com/gitlwh/FUSE-Filesystem'
-	}, {
-		title: 'Loyal Customer Prediction',
-		image: 'dist/images/loyalCustomer.jpg',
-		description: 'The challenge of this competition was to predict which shoppers that responded to a rebate coupon on a specific period, would become repeat buyers of that product. This is code to generate my submission to the Kaggle competition - Loyal Customer Prediction.',
-		keywords: 'Kaggle, top5%, Python, Numpy, Scipy, Pandas, Xgboost, Sklearn, Vowpal Wabbit',
-		demo: 'https://inclass.kaggle.com/c/loyal-customer-prediction',
-		source: 'https://github.com/gitlwh/Loyal-Customer-Prediction'
-	}, {
 		title: 'Event Planner',
 		image: 'dist/images/eventPlanner.jpeg',
 		description: 'This is an event planner which can build a record about the place, people, time, note of things you are going to do.',
@@ -55,22 +30,46 @@ $(function() {
 		description: 'This is a website that enabling different user to log in, configure profile, store and search passwords, built on framework of Codeigniter. As a project during intern, the source code is unavaliable here! The website is running on inner server, the demo is unavaliable as well!',
 		keywords:'Codeigniter, JavaScript, HTML, CSS, AngularJS, jQuery'
 	}, {
+		title: 'Botnet Detector',
+		image: 'dist/images/botnetDetector.jpg',
+		description: 'A tool used to detect botnet based on existing P2P botnet packet dataset and health packet dataset. Using machine learning to differentiate botnet trace out of normal trace.',
+		keywords:'Python, Tshark, Numpy, Sklearn',
+		source: 'https://github.com/gitlwh/Botnet-Detector'
+	}, {
+		title: 'User Level Threading Library',
+		image: 'dist/images/userLevelThreadingLibrary.jpg',
+		description: 'A user level threading library with function of create, wait, join, exit, etc.',
+		keywords:'C/C++',
+		source: 'https://github.com/gitlwh/User-Level-Threading-Library'
+	}, {
+		title: 'FUSE Filesystem',
+		image: 'dist/images/FUSEFilesystem.jpg',
+		keywords: 'C/C++',
+		description: 'A file system with the FUSE Userspace filesystem module.',
+		source: 'https://github.com/gitlwh/FUSE-Filesystem'
+	}, {
 		title: 'File Server',
 		image: 'dist/images/fileServer.jpg',
 		description: 'This is an server which enables user to upload and download file in multiple secure methods.',
 		keywords:'C/C++, RSA, Openssl, md5, Socket',
 		source: 'https://github.com/gitlwh/File-server'
+	}, {
+		title: 'Loyal Customer Prediction',
+		image: 'dist/images/loyalCustomer.jpg',
+		description: 'The challenge of this competition was to predict which shoppers that responded to a rebate coupon on a specific period, would become repeat buyers of that product. This is code to generate my submission to the Kaggle competition - Loyal Customer Prediction.',
+		keywords: 'Kaggle, top5%, Python, Numpy, Scipy, Pandas, Xgboost, Sklearn, Vowpal Wabbit',
+		demo: 'https://inclass.kaggle.com/c/loyal-customer-prediction',
+		source: 'https://github.com/gitlwh/Loyal-Customer-Prediction'
 	}];
 
 	var fullStackProjects = [
 	{
 		title: 'An accurate power-sharing control method based on circulating-current power model for voltage-source-inverter parallel system',
-		image: '',
+		image: 'dist/images/paper.jpeg',
 		description: 'Published in 2015 IEEE Applied Power Electronics Conference and Exposition (APEC). This paper analyzes the voltage-source-inverter parallel system as a multi-input & multi-output system, and proposes the circulating-current mathematical model, the steady-state model and small-signal model of parallel system. Then the phasor models of the circulating-current and the circulating-current power are proposed. Subsequently, the mathematical model of traditional droop control is built and analyzed based on the circulating-current power model. According to the analysis of droop control, this paper proposes an accurate parallel control strategy (ω-Pcir and V-Qcir control), which can eliminate the circulating-current power and the bias of output voltage frequency and amplitude. Finally, simulation and experiment result are presented, which validates the performance of the proposed method.',
-		keywords:'',
-		demo: '',
-		code: '',
-		type: ''
+		keywords:'Electrical Electronic, Voltage-source Inverter, Parallel system, Droop control, Circulating-current',
+		demo: 'files/paper.pdf',
+		code: ' '
 	},{
 		title: '[CN]Multiple Valve Regulation of the Boiler Feed Water System',
 		image: 'dist/images/BoilerFeedWaterSystem.jpeg',
@@ -158,31 +157,31 @@ $(function() {
 
 	var mediaworks = [{
 		image: 'dist/images/cover.jpg',
-		description: 'Cover for culture promotion team handbook.'
+		description: '[Photoshop] Cover for culture promotion team handbook. '
 	},{
 		image: 'dist/images/badge.jpg',
-		description: 'Badge for culture promotion team seventh anniversary.'
-	},{
-		image: 'dist/images/fun.jpeg',
-		description: 'Works for Zhejiang University Industrial Design competition'
-	},{
-		image: 'dist/images/flashlight.jpeg',
-		description: 'Works for Zhejiang University Industrial Design competition'
+		description: '[Photoshop] Badge for culture promotion team seventh anniversary.'
 	},{
 		image: 'dist/images/Memorial.bmp',
-		description: 'Memorial mail for Zhejiang University campuses travel.'
-	},{
-		image: 'dist/images/chair.jpg',
-		description: 'Works for Zhejiang University Industrial Design competition.'
+		description: '[Photoshop] Memorial mail for Zhejiang University campuses travel.'
 	},{
 		image: 'dist/images/Memorial2.jpg',
-		description: 'Memorial mail for Zhejiang University campuses travel.'
+		description: '[Photoshop] Memorial mail for Zhejiang University campuses travel.'
 	},{
 		image: 'dist/images/head.png',
-		description: 'A profile image showing a bird burning in the sun and struggling to survive.'
+		description: '[Photoshop] A profile image showing a bird burning in the sun and struggling to survive.'
+	},{
+		image: 'dist/images/fun.jpeg',
+		description: '[Photoshop, Rhino] Works for Zhejiang University Industrial Design competition'
+	},{
+		image: 'dist/images/flashlight.jpeg',
+		description: '[Photoshop, Rhino] Works for Zhejiang University Industrial Design competition'
+	},{
+		image: 'dist/images/chair.jpg',
+		description: '[Rhino] Works for Zhejiang University Industrial Design competition.'
 	},{
 		image: 'dist/images/Microcinema.jpeg',
-		description: 'A 5-min Microcinema taken by Nokia N8 for a Microcinema competition.',
+		description: '[Adobe Premiere Pro] A 5-min Microcinema taken by Nokia N8 for a Phone Microcinema competition.',
 		demo:"https://www.youtube.com/watch?v=iF8GV96w8eo"
 	
 	}];
